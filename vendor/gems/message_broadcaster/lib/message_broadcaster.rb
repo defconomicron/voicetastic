8befa85bf0407a3d4dbc08cd85b2a0dada6b2187
Adam Hernandez <adam@digimedia.com>
2025-04-17 15:22:18 -0500

    .

--- vendor/gems/message_broadcaster/lib/message_broadcaster.rb
+++ vendor/gems/message_broadcaster/lib/message_broadcaster.rb
@@ -3,16 +3,27 @@ class MessageBroadcaster
   end
 
   def broadcast(ch_index: nil, message: nil, node: nil, voice: nil)
-    channel_name = ch_index.present? ? channels[ch_index.to_i] : nil
-    tokens1 = []
-    tokens1 << "[#{channel_name}]" if channel_name.present?
-    tokens1 << "&lt;#{node.nil? ? 'SYSTEM' : node.name}&gt;"
-    tokens2 = []
-    tokens2 << message
-    tokens2 << TextToVoice.new(message).voice if voice
-    html = LiElement.new(tokens1.join(' ') << ' ' << tokens2.join('<br />')).render
-    Message.new.broadcast_prepend_to('Messages', target: 'messages', html: html)
+    str = [ch_index_to_channel_name(ch_index), node_to_node_name(node)].join(' ') << ' ' <<
+          [message, text_to_voice(message, voice)].join('<br />')
+    Message.new.broadcast_prepend_to('Messages', target: 'messages', html: str_to_li_element(str))
     self
+  end
+
+  def ch_index_to_channel_name(ch_index)
+    str = ch_index.present? ? channels[ch_index.to_i] : nil
+    "[#{str}]" if str.present?
+  end
+
+  def node_to_node_name(node)
+    "&lt;#{node.nil? ? 'SYSTEM' : node.name}&gt;"
+  end
+
+  def text_to_voice(message, voice)
+    TextToVoice.new(message).voice if voice
+  end
+
+  def str_to_li_element(str)
+    LiElement.new("#{str}").render
   end
 
   def channels
