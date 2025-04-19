d98a58190cc1185a8383df55a263eb8381b0c657
Adam Hernandez <adam@digimedia.com>
2025-04-18 21:47:17 -0500

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # before_action :set_cache_headers
+
+  private
+
+    # def set_cache_headers
+    #   response.headers['Cache-Control'] = 'no-cache, no-store'
+    #   response.headers['Pragma'] = 'no-cache'
+    #   response.headers['Expires'] = 'Mon, 01 Jan 1990 00:00:00 GMT'
+    # end
 end
